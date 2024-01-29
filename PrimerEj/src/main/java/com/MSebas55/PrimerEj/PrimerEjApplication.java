6d1d9f8f5e649058a27eb9aa477e9a7e51f81e16
MSebas55 <calimandrucsebas@gmail.com>
2024-01-29 15:44:42 +0100

    creado primer ej de intelij

---
+++ PrimerEj/src/main/java/com/MSebas55/PrimerEj/PrimerEjApplication.java
@@ -0,0 +1,13 @@
+package com.MSebas55.PrimerEj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerEjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerEjApplication.class, args);
+	}
+
+}
---
+++ PrimerEj/src/test/java/com/MSebas55/PrimerEj/PrimerEjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MSebas55.PrimerEj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerEjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
